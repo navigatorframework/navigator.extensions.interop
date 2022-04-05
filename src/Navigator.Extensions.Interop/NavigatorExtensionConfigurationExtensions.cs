1a1810b5a0ab4a4e46aa3e21a5077ce024bc169e
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-04-05 16:59:15 +0200

    feat: first iteration of configuration

--- src/Navigator.Extensions.Interop/NavigatorExtensionConfigurationExtensions.cs
+++ src/Navigator.Extensions.Interop/NavigatorExtensionConfigurationExtensions.cs
@@ -1,7 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Navigator.Actions;
 using Navigator.Configuration;
 using Navigator.Configuration.Extension;
-using Navigator.Context.Extensions;
-using Python.Included;
+using Navigator.Context;
+using Python.Runtime;
 
 namespace Navigator.Extensions.Interop;
 
@@ -9,10 +13,53 @@ public static class NavigatorExtensionConfigurationExtensions
 {
     public static NavigatorConfiguration Interop(this NavigatorExtensionConfiguration configuration)
     {
-        Installer.SetupPython().GetAwaiter().GetResult();
+        try
+        {
+            Runtime.PythonDLL = "libpython3.so";
+            PythonEngine.Initialize();
+            PythonEngine.BeginAllowThreads();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
 
-        return configuration.Extension(
-            _ => {},
-            services => {});
+        return configuration.Extension(configuration =>
+        {
+            configuration.Services.LoadAllInteropActions();
+
+            var a = typeof(InteropAction).IsAssignableTo(typeof(IAction));
+
+            var actions = configuration.Services
+                .Where(descriptor => descriptor.ImplementationType?.IsAssignableTo(typeof(IAction)) 
+                                     ?? descriptor.ImplementationFactory?.Method.ReturnType.IsAssignableTo(typeof(IAction))
+                                     ?? false)
+                .Select(descriptor => descriptor.ImplementationType ?? descriptor.ImplementationFactory!.Method.ReturnType)
+                .ToArray();
+            
+            configuration.Options.RegisterActionsCore(actions, true);
+        
+            configuration.Options.RegisterActionsPriorityCore(actions, true);
+        });
+    }
+
+    private static void LoadAllInteropActions(this IServiceCollection services)
+    {
+        foreach(var modulePath in Directory.EnumerateFiles(Directory.GetCurrentDirectory(), "*.py", SearchOption.AllDirectories))
+        {
+            var path = Path.GetDirectoryName(modulePath);
+            var name = Path.GetFileNameWithoutExtension(modulePath);
+
+            if (path is not null)
+            {
+                services.AddScoped<InteropAction, InteropAction>(provider =>
+                {
+                    var accessor = provider.GetRequiredService<INavigatorContextAccessor>();
+
+                    return new InteropAction(accessor, (path, name));
+                });
+            }
+        }
     }
 }
